893696ac7666602d8bda390532f8e485cd00b019
Chris Closser <chrisclosser1@gmail.com>
2017-10-25 17:33:18 -0700

    working on dll push

--- dll.py
+++ dll.py
@@ -1,6 +1,6 @@
 """Implementation of double linked list."""
 
-from linked_list import LinkedList
+from linked_list import LinkedList, Node
 
 
 class DLL(LinkedList):
@@ -11,10 +11,33 @@ class DLL(LinkedList):
         super(DLL, self).__init__()
         self.tail = None
 
-    # def push(self, val):
-    #     """Add new item to the head of the double linked list."""
-    #     super(DLL, self).push(val)
+    def push(self, val):
+        """Add new item to the head of the double linked list."""
+        # super(DLL, self).push(val)
+        if not self.head:
+            orig_head = self.head
+            self.head = Node(val, self.head)
+            orig_head.prev = self.head
+        else:
+            self.head = Node(val, self.head)
+
+        self.length += 1
 
     def pop(self):
         """Remove and return the head."""
         return super(DLL, self).pop()
+
+    def remove(self, node):
+        """Remove selected node."""
+        current = self.head
+        node_to_remove = None
+        while current:
+            if current.next.val == node:
+                node_to_remove = current.next
+                self.length -= 1
+                current.next = current.next.next
+                current.prev = current.prev.prev
+            current = current.next
+
+        if node_to_remove is None:
+            raise IndexError("Node was not found in list")
--- test_dll.py
+++ test_dll.py
@@ -17,7 +17,7 @@ def test_dll_on_init_has_tail(new_dll):
     assert hasattr(new_dll, 'tail')
 
 
-def test_push_to_dll(new_dll):
+    def test_push_to_dll(new_dll):
     """Test to for push method."""
     new_dll.push(2)
     assert new_dll.head.val == 2
@@ -27,9 +27,18 @@ def test_node_has_prev_pointer():
     """Test for node previous pointer."""
     test_node = Node(1)
     assert test_node.prev is None
+#
+#
+# def test_pop_returns_removes_and_returns_head(new_dll):
+#     """Test_pop_returns_removes_and_returns_head."""
+#     new_dll.push(4)
+#     assert new_dll.pop() == 4
+#
 
-
-def test_pop_returns_removes_and_returns_head(new_dll):
-    """Test_pop_returns_removes_and_returns_head."""
-    new_dll.push(4)
-    assert new_dll.pop() == 4
+# def test_remove_node_from_list(new_dll):
+#     """Test_remove_node_from_list."""
+#     new_dll.push(1)
+#     new_dll.push(2)
+#     new_dll.push(3)
+#     new_dll.remove(2)
+#     assert new_dll.head.next.val == 1
