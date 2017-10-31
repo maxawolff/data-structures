3de59d698a55af2cb4a34098f5bc2c491da71108
maxawolff <maxawolff@hotmail.com>
2017-10-31 16:36:26 -0700

    added another test

--- test_binary_heap.py
+++ test_binary_heap.py
@@ -79,3 +79,24 @@ def test_ten_vals_backwards(new_bin):
     new_bin.push(10)
     print(new_bin.container)
     assert new_bin.container == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+def test_15_vals_backwards(new_bin):
+    """."""
+    new_bin.push(1)
+    new_bin.push(2)
+    new_bin.push(3)
+    new_bin.push(4)
+    new_bin.push(5)
+    new_bin.push(6)
+    new_bin.push(7)
+    new_bin.push(8)
+    new_bin.push(9)
+    new_bin.push(10)
+    new_bin.push(11)
+    new_bin.push(12)
+    new_bin.push(13)
+    new_bin.push(14)
+    new_bin.push(15)
+    print(new_bin.container)
+    assert new_bin.container == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
