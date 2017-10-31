3a2e2e3d6e2f3a488bab8b3a65f419f26d61243c
Chris Closser <chrisclosser1@gmail.com>
2017-10-31 14:05:49 -0700

    class init binary tree

---
+++ binary_heap.py
@@ -0,0 +1,11 @@
+"""Implementation of binary search tree."""
+
+
+class Binheap():
+    """Binary search tree."""
+
+    def __init__(self):
+        """."""
+        self.head = None
+        self.parent = None
+        self.chideren = []
