26266d73743ec14a955e841c532ba08ba8ecdd9c
Chris Closser <chrisclosser1@gmail.com>
2017-11-01 14:44:48 -0700

    starting priorityq

---
+++ priorityq.py
@@ -0,0 +1,20 @@
+"""."""
+
+from dll import DLL
+
+class Priorityq(DLL):
+    """."""
+
+    def insert(self, val, priority):
+        """."""
+        super(Priorityq, self).append(val, priority=priority)
+        current = self.tail
+        while current.prev:
+            
+
+    def pop(self):
+        """."""
+
+
+    def peek():
+        """."""
