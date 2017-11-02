79ca76d493c3f7620f52ed56a5c5f960d3b5ff66
maxawolff <maxawolff@hotmail.com>
2017-11-02 16:44:25 -0700

    still working on it

--- priorityq.py
+++ priorityq.py
@@ -13,6 +13,8 @@ class Priorityq(DLL):
         # if self.length > 1:
         #     current = self.tail
         current = self.tail
+        if current == self.head:
+            return
         if current.prev_node is self.head:
             if current.prev_node.priority < current.priority:
                 old_prev = current.prev_node
@@ -24,42 +26,53 @@ class Priorityq(DLL):
                 self.tail = old_prev
                 self.head = current
                 return
-
+        count = 0
+        pdb.set_trace()
         while current.prev_node:
+            pdb.set_trace()
             if current.prev_node.priority < current.priority:
                 # pdb.set_trace()
+                count += 1
+                print('in while loop, count is ', count)
                 old_prev = current.prev_node
                 # old_prev, current = current, old_prev
                 if current.next_node is None:  # current is tail
                     old_prev.next_node = None
-                    if old_prev.prev_node:
+                    if current.prev_node.prev_node:
                         current.next_node = old_prev
                         old_prev.prev_node.next_node = current
                         current.prev_node = current.prev_node.prev_node
                         old_prev.prev_node = current
                         current.next_node = old_prev
                         self.tail = old_prev
-                        current = current.prev_node
+                        current = current.prev_node  # current is being reasigned to incorect value
+                        print(self, 'in if current.next is none')
                         continue
-                # elif current.prev_node is None:
-
                     else:
-                        current.prev_node = None
-                        self.head = current
-                        break
-                elif old_prev.prev_node:
-                        current.prev_node = old_prev.prev_node
-                        old_prev.prev_node = current
-                        current.next_node = old_prev
-                        continue
-                        # self.tail = old_prev
-                else:
+                        return
+                elif current.prev_node.prev_node:
+                    current.next_node.prev_node = old_prev
+                    current.prev_node = old_prev.prev_node
+                    old_prev.prev_node.next_node = current
+                    old_prev.prev_node = current
+                    current.next_node = old_prev
+                    old_prev.next_node = old_prev.next_node.next_node
                     current = current.prev_node
-                #     # current.prev_node = None
-                #     # self.head = current
+                    print(self, "haven't hit head yet")
                     continue
+                elif current.prev_node.prev_node is None:
+                    current.next_node.prev_node = old_prev
+                    current.prev_node = None
+                    old_prev.prev_node = current
+                    current.next_node = old_prev
+                    old_prev.next_node = old_prev.next_node.next_node
+                    self.head = current
+                    print(self, 'just reasigned head')
+                    return
+                else:
+                    return
             else:
-                break
+                return
 
     def peek(self):
         """."""
