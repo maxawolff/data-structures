9a069f7dece0d4917e8acf7712ad47784b80e0f2
maxawolff <maxawolff@hotmail.com>
2017-11-02 14:01:54 -0700

    still working on insert

--- priorityq.py
+++ priorityq.py
@@ -10,22 +10,48 @@ class Priorityq(DLL):
         """."""
         super(Priorityq, self).append(val, priority=priority)
         import pdb  # pdb.set_trace()
-        if self.length > 1:
-            current = self.tail
-
-            while current.prev_node:
-                if current.prev_node.priority < current.priority:
-                    old_prev = current.prev_node
-                    old_prev, current = current, old_prev
-                    if old_prev == self.tail:
-
-                        self.tail = current
-                    if current.prev_node is None:
-                        self.head = old_prev
-                    current = old_prev.prev_node
-                    pdb.set_trace()
+        # if self.length > 1:
+        #     current = self.tail
+        current = self.tail
+        while current.prev_node:
+            pdb.set_trace()
+            if current.prev_node.priority < current.priority:
+                old_prev = current.prev_node
+                # old_prev, current = current, old_prev
+                if current.next_node is None:  # current is tail
+                    old_prev.next_node = None
+                    self.tail = old_prev
+                    current.next_node = old_prev
+                    if old_prev.prev_node:
+                        current.prev_node = old_prev.prev_node
+                        current = current.prev_node
+                        continue
+                    else:
+                        current.prev_node = None
+                        self.head = current
+                        break
                 else:
-                    break
+                    current.next_node = old_prev
+                    if old_prev.prev_node:
+                        current.prev_node = old_prev.prev_node
+                        current = current.prev_node
+                        continue
+                    else:
+                        current.prev_node = None
+                        self.head = current
+                        break
+
+                #     old_prev.prev_node = current
+                #     if
+                #     self.tail = old_prev
+                # if current.prev_node is None: # current is head
+                #     old_prev.prev_node = None
+                #     self.head = old_prev
+                # current.next_node = old_prev
+                # old_prev.prev_node = current
+                # current = old_prev.prev_node
+            else:
+                break
 
     def peek(self):
         """."""
