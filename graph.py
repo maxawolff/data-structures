c6816c530bc9affa3bf80750210ffc8d15fbfccf
maxawolff <maxawolff@hotmail.com>
2017-11-03 14:43:04 -0700

    added add_edge, started testing

--- graph.py
+++ graph.py
@@ -3,6 +3,7 @@
 
 class Graph(object):
     """Graph data structure."""
+
     def __init__(self):
         """."""
         self._nodes = []
@@ -18,10 +19,31 @@ class Graph(object):
 
     def add_node(self, val):
         """."""
+        for node in self._nodes:
+            if node.val == val:
+                raise ValueError("Nodes must have unique values")
         self._nodes.append(Node(val))
 
     def add_edge(self, val1, val2):
-        """."""
+        """Add a connection between two nodes, val1 points to val2."""
+        node1 = 0
+        node2 = 0
+        for node in self._nodes:
+            if node.val == val1:
+                node1 = node
+            if node.val == val2:
+                node2 = node
+        if node1 == 0:
+            node1 = Node(val1)
+            self.add_node(node1.val)
+        if node2 == 0:
+            node2 = Node(val2)
+            self.add_node(node2.val)
+        if node1 == node2:
+            raise ValueError("You cannot connect a node to itself")
+        node1.neighbors.append((node1, node2))
+        node2.neighbors.append((node1, node2))
+        self._edges.append((node1, node2))
 
     def del_node(self, val):
         """."""
@@ -41,6 +63,7 @@ class Graph(object):
 
 class Node(object):
     """Graph node."""
+
     def __init__(self, val, neighbors=[]):
         """."""
         self.val = val
--- test_graph.py
+++ test_graph.py
@@ -12,6 +12,7 @@ def new_graph():
 
 @pytest.fixture
 def new_node():
+    """A node."""
     return Node(1)
 
 
@@ -39,3 +40,23 @@ def test_insert_1node(new_graph):
 def test_newgraph_scope(new_graph):
     """Test fixture."""
     assert new_graph._nodes == []
+
+
+def test_insert_duplicate_node_raises_error(new_graph):
+    """."""
+    new_graph.add_node(1)
+    with pytest.raises(ValueError):
+        new_graph.add_node(1)
+
+
+def test_add_edge_empty_graph_makes_nodes(new_graph):
+    """Should make two new nodes and add to nodes list."""
+    new_graph.add_edge(1, 2)
+    assert len(new_graph._nodes) == 2
+
+
+def test_add_edge_empty_graph_makes_edge(new_graph):
+    """Should add an edge."""
+    new_graph.add_edge(1, 2)
+    assert new_graph._edges[0][0].val == 1
+    assert new_graph._edges[0][1].val == 2
