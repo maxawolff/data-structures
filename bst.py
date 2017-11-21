20f59bfc9f61ff1836f133be681c0900ad9969db
maxawolff <maxawolff@hotmail.com>
2017-11-21 13:42:15 -0800

    adding depth

--- bst.py
+++ bst.py
@@ -6,11 +6,12 @@ import pdb
 class Node(object):
     """Node class for binary search tree."""
 
-    def __init__(self, value, left=None, right=None):
+    def __init__(self, value, left=None, right=None, depth=0):
         """."""
         self.value = value
         self.left = left
         self.right = right
+        self.depth = 0
 
 
 class BST(object):
@@ -19,14 +20,17 @@ class BST(object):
     def __init__(self, iterable=None):
         """."""
         self.head = None
+        self.left_depth = 0
+        self.right_depth = 0
 
     def insert(self, val):
         """Add a node into the bst."""
         if self.head is None:
-            self.head = Node(val)
+            self.head = Node(val, depth=1)
         current_node = self.head
         if val == self.head.value:
             return
+        current_depth = 2
         while True:
             if val < current_node.value:
                 if current_node.left:
