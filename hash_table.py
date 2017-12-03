0789a782d38b19bb30179111c6cd4cc625ebb807
maxawolff <maxawolff@hotmail.com>
2017-12-03 11:02:46 -0800

    wrote set function for hash table

--- hash_table.py
+++ hash_table.py
@@ -4,12 +4,14 @@
 class HashTable(object):
     """class for Hash_Table."""
 
-    def __init__(self, size):
+    def __init__(self, size, func='None'):
         """Build hash table on initiation."""
         self.size = size
         self.container = []
         for i in range(0, self.size):
-            self.container.append([])
+            self.container.append({})
+        if func is None:
+            self.func = self._additive
 
     def _additive(self, key):
         """Given a key, perform a hashing algorithm on it."""
@@ -17,3 +19,12 @@ class HashTable(object):
         for letter in key:
             value += ord(letter)
         return value % self.size
+
+    def set(self, key, val):
+        """Set a value in the hash table."""
+        if not type(key) == str:
+            raise TypeError('key must be a string')
+        bucket = self.func(key)
+        self.container[bucket]['key'] = val
+
+
