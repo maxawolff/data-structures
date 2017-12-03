b1cc6586cdfbe9617fa2ad0ae4ab26f820b71aaa
maxawolff <maxawolff@hotmail.com>
2017-12-03 11:21:36 -0800

    finished hash table for additive hash, started testing

--- hash_table.py
+++ hash_table.py
@@ -4,7 +4,7 @@
 class HashTable(object):
     """class for Hash_Table."""
 
-    def __init__(self, size, func='None'):
+    def __init__(self, size, func=None):
         """Build hash table on initiation."""
         self.size = size
         self.container = []
@@ -12,6 +12,8 @@ class HashTable(object):
             self.container.append({})
         if func is None:
             self.func = self._additive
+        else:
+            self.func = func
 
     def _additive(self, key):
         """Given a key, perform a hashing algorithm on it."""
@@ -20,11 +22,24 @@ class HashTable(object):
             value += ord(letter)
         return value % self.size
 
+    def _hash(self, key):
+        """Hash a given key."""
+        bucket = self.func(key)
+        return bucket
+
     def set(self, key, val):
         """Set a value in the hash table."""
         if not type(key) == str:
             raise TypeError('key must be a string')
-        bucket = self.func(key)
-        self.container[bucket]['key'] = val
+        bucket = self._hash(key)
+        self.container[bucket][key] = val
 
-
+    def get(self, key):
+        """Get the value for a given key."""
+        if not type(key) == str:
+            raise TypeError('key must be a string')
+        bucket = self.hash(key)
+        try:
+            return self.container[bucket][key]
+        except KeyError:
+            return None
--- test_hash_table.py
+++ test_hash_table.py
@@ -2,15 +2,27 @@
 
 import pytest
 from hash_table import HashTable
+import pdb
 
 
-def test_init():
+@pytest.fixture
+def ht():
+    """Make graph to play with."""
+    return HashTable(10)
+
+
+def test_init(ht):
     """Test that buckets get made on init."""
-    h = HashTable(10)
-    assert len(h.container) == 10
+    assert len(ht.container) == 10
 
 
-def test_additive():
+def test_additive(ht):
     """Test that additive properly hashes a value."""
-    h = HashTable(10)
-    assert h._additive('key') == 9
+    assert ht._additive('key') == 9
+
+
+def test_set_adds_value(ht):
+    """Set should properly store a value at a given key."""
+    ht.set('foo', 10)
+    bucket = ht._additive('foo')
+    assert 'foo' in ht.container[bucket]
