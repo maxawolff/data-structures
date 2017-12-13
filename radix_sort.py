7346b1000d751657d22cb17700951ed4586d8f31
maxawolff <maxawolff@hotmail.com>
2017-12-12 17:46:57 -0800

    first attempt at final sort is incorrect, take 2 incoming

--- radix_sort.py
+++ radix_sort.py
@@ -1,5 +1,6 @@
 from que_ import Queue
 from math import log10
+import pdb
 
 
 def radix_sort(input):
@@ -11,7 +12,29 @@ def radix_sort(input):
     for num in input:
         if num > largest:
             largest = num
-    length = int(log10(largest))
+    length = int(log10(largest)) + 1
     for num in input:
-        container.append(list(map(int, str(num)))[::-1])
+        str_num = str(num)
+        str_num_list = []
+        for i in range(0, length):
+            try:
+                str_num_list.append(int(str_num[i]))
+            except IndexError:
+                str_num_list.append(0)
+        container.append(str_num_list)
+    pdb.set_trace()
+    for i in range(0, length):
+        bucket_list = []
+        for j in range(0, 10):
+            q = Queue()
+            q.enqueue(j)
+            bucket_list.append(q)
+        for num in container:
+            digit = num[i]
+            bucket_list[digit].enqueue(num)
+        container = []
+        for bucket in bucket_list:
+            bucket.dequeue()
+            while bucket:
+                container.append(bucket.dequeue())
     return container
