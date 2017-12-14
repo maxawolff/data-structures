f3f5a579033e2a697b3dfe410df3dbc6a30f382b
maxawolff <maxawolff@hotmail.com>
2017-12-14 15:32:00 -0800

    quick sort is done, recursion was the way to go, tough to get my head around it but it ended up being really simple

--- quick_sort.py
+++ quick_sort.py
@@ -3,15 +3,15 @@
 done = []
 
 
-def quick_sort(input, start, end):
+def quick_sort(input):
     """Sort a list using the quick sort algorith."""
     if not isinstance(input, list):
         raise TypeError("quick sort only works on a list")
     if len(input) == 1:
         return input
-    left = start
-    right = start
-    for i in range(start, end):
+    left = 0
+    right = 0
+    for i in range(len(input)):
         if right == 0 and input[i] > input[0]:
             right = i
         elif input[i] <= input[0]:
@@ -20,12 +20,12 @@ def quick_sort(input, start, end):
             input[left], input[right] = input[right], input[left]
             left = right
             right += 1
-    input[start], input[left] = input[left], input[start]
-    done.append(left)
-    return input
-
-
-def qs(input):
-    """Run quick sort as much as neccesary."""
-    if not done:
-        quick_sort(input, 0, 10)
+    input[0], input[left] = input[left], input[0]
+    left_list = input[:left]
+    if len(left_list) > 1:
+        left_list = quick_sort(left_list)
+    middle_list = [input[left]]
+    right_list = input[left + 1:]
+    if len(right_list) > 1:
+        right_list = quick_sort(right_list)
+    return left_list + middle_list + right_list
