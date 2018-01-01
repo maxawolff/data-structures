ca76f2858fb18d2fba28ce1e7f1f8738ea78987a
maxawolff <maxawolff@hotmail.com>
2017-12-31 19:24:21 -0800

    cleaned up variable names

--- test_quick_sort.py
+++ test_quick_sort.py
@@ -29,9 +29,9 @@ def test_quicksort_in_order():
 
 def test_quicksort_in_reversed_order():
     """Test quicksort works on an in order list."""
-    input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    output = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    assert quick_sort(output) == input
+    output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    input = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    assert quick_sort(input) == output
 
 
 def test_quicksort_giant_list():
